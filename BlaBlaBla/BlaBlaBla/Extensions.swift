ddf15a486e209bf22cc3b04eae30deb941f27a77
Camila Resende <aliamcami@gmail.com>
2018-04-06 18:40:37 -0300

    Initial setup

---
+++ BlaBlaBla/BlaBlaBla/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  BlaBlaBla
+//
+//  Created by camila oliveira on 06/04/18.
+//  Copyright © 2018 Aliamcami. All rights reserved.
+//
+
+import UIKit
+
+class Extensions: NSObject {
+
+}
---
+++ BlaBlaBla/BlaBlaBlaPlaygound/BlaBlaBlaPlaygound.h
@@ -0,0 +1,19 @@
+//
+//  BlaBlaBlaPlaygound.h
+//  BlaBlaBlaPlaygound
+//
+//  Created by camila oliveira on 06/04/18.
+//  Copyright © 2018 Aliamcami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BlaBlaBlaPlaygound.
+FOUNDATION_EXPORT double BlaBlaBlaPlaygoundVersionNumber;
+
+//! Project version string for BlaBlaBlaPlaygound.
+FOUNDATION_EXPORT const unsigned char BlaBlaBlaPlaygoundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlaBlaBlaPlaygound/PublicHeader.h>
+
+
