dc9724ee2b4cba87cc2eb5ad333edd1fefe4462f
Camila Resende <aliamcami@gmail.com>
2018-04-08 23:08:21 -0300

    lots of things and ptbr done

--- BlaBlaBla/BlaBlaBla/PtBrSentenceGenerator.swift
+++ BlaBlaBla/BlaBlaBla/PtBrSentenceGenerator.swift
@@ -12,14 +12,15 @@ class PtBrSentenceGenerator: SentenceMaker {
     fileprivate static var wordList: WordList = PtBrWordList()
     
     open static func random() -> String {
-        return [randomBeingStart(), randomBeingInCountry(), randomBeingInPlace(), randomBeingWithObject(), randomBeingWithObject(), beingInPlaceFromCountry()].random
+        //randomAdjective(), randomInCountry(), randomAtPlace(),,randomAdjectiveLike()
+        return [randomBeingStart(addAdjective: true),randomBeingInCountry(), randomBeingInPlace(), randomBeingWithObject(), randomBeingWithObject(),randomCountryWithAnimal(),randomPlaceWith()].random
     }
     
     //MARK:- sentence generator
-    //MARK: general
+    //MARK: you are an being...
     ///Returns setence for "you are an 'being'", if you do not pass on argument it will get an random being
-    open static func randomBeingStart(_ being: String? = nil) -> String {
-        let being = being ?? animalWithAdjective
+    open static func randomBeingStart(_ being: String? = nil, addAdjective: Bool? = nil) -> String {
+        let being = being ?? animalWithRandomAdjective(addRandomAdjective: addAdjective)
         return "Você é ".concat(with: being).capitalizingFirstLetter()
     }
  
@@ -29,38 +30,68 @@ class PtBrSentenceGenerator: SentenceMaker {
     }
     
     open static func randomBeingInPlace(being: String? = nil, place: String? = nil) -> String {
-        let str = ["em", "indo para", "saindo de"].random.concat(with: place ?? wordList.places.random)
+        let str = ["em", "indo para", "saindo de"].random.concat(with: place ?? placeFromCountry())
         return randomBeingStart(being).concat(with: str).capitalizingFirstLetter()
     }
     
     open static func randomBeingWithObject(being: String? = nil, object: String? = nil) -> String{
-        let str = ["e está segurando", "e está mordendo", "com", "e está fazendo", " olhando para"].random.concat(with: object ?? wordList.objects.random)
+        let str = ["e está segurando", "e está mordendo", "com", " olhando para"].random.concat(with: object ?? wordList.objects.random)
         return randomBeingStart(being).concat(with: str).capitalizingFirstLetter()
     }
     
-    open static func beingInPlaceFromCountry(being:String? = nil, place: String? = nil, from: String? = nil) -> String {
-        var country = from ?? wordList.countries.random
-        country = "d" + country.trimmingCharacters(in: .whitespaces).dropFirst()
-        return randomBeingInPlace(being: being, place: place).concat(with: country).capitalizingFirstLetter()
+    
+    //MARK: you are at...
+    open static func randomInCountry(_ country: String? = nil) -> String{
+        return "Você está".concat(with: country ?? wordList.countries.random).capitalizingFirstLetter()
     }
+    open static func randomAtPlace(_ place: String? = nil) -> String{
+        return "Você está em".concat(with: place ?? placeFromCountry()).capitalizingFirstLetter()
+    }
+    open static func randomAdjective(_ adjective: String? = nil) -> String{
+        return "Você \(["está", "é"].random)".concat(with: adjective ?? wordList.adjectives.random).capitalizingFirstLetter()
+    }
+    open static func randomAdjectiveLike(adjective: String? = nil, likeBeing: String? = nil) -> String{
+        return randomAdjective(adjective).concat(with: "como").concat(with: likeBeing ?? wordList.animals.random).capitalizingFirstLetter()
+    }
+    open static func randomCountryWithAnimal(country: String? = nil, animal: String? = nil) -> String{
+        return randomInCountry(country).concat(with: ["com", "acompanhando", "vigiando", "observando"].random).concat(with: animal ?? animalWithRandomAdjective()).capitalizingFirstLetter()
+    }
+    open static func randomPlaceWith(place: String? = nil, with thing: String? = nil) -> String{
+        return randomAtPlace().concat(with: ["com", "olhando", "vigiando", "observadno"].random
+            ).concat(with: thing ?? [animalWithRandomAdjective(), wordList.objects.random].random).capitalizingFirstLetter()
+    }
+   
+
+    
     
     //MARK:- usefull
-    static var animalWithAdjective: String{
-        return beingWithAdjective(being: wordList.animals.random)
+    static func animalWithRandomAdjective(addRandomAdjective add: Bool? = nil)-> String{
+        return beingWithRandomAdjective(being: wordList.animals.random, addAdjective: add ?? [true,false].random)
     }
     static var objectWithAdjective: String{
-        return beingWithAdjective(being: wordList.objects.random)
+        return beingWithRandomAdjective(being: wordList.objects.random, addAdjective: [true,false].random)
     }
     
-    static func beingWithAdjective(being: String? = nil, adjective: String? = nil)-> String{
+    static func beingWithRandomAdjective(being: String? = nil, adjective: String? = nil, addAdjective: Bool)-> String{
         let being = being ?? [wordList.animals.random, wordList.objects.random].random
         
-        if [true,false].random {
+        if addAdjective {
             let adj = gender(fix: adjective ?? wordList.adjectives.random, based: being)
             return being.concat(with: adj)
         }
         
         return being
+    }
+    
+    static func placeFromCountry(place: String? = nil, country: String? = nil)-> String{
+        let str = place ?? wordList.places.random
+        
+        if [true, false].random{
+            let country = "d" + (country ?? wordList.countries.random).trimmingCharacters(in: .whitespaces).dropFirst()
+            return str.concat(with: country)
+        }
+        
+        return str
     }
     
     fileprivate static func startWithUma(str: String)-> Bool{
